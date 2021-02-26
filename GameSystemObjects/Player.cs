cacfa962a3da829384c0438b9ae0ad389d8efcbc
Bailey <bapcos@gmail.com>
2021-02-25 20:53:04 -0500

    Init commit
    All teh files

---
+++ GameSystemObjects/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSystemObjects
+{
+
+    public class Player
+    {
+        private string name;
+
+        private List<ItemTask> items;
+
+        public Player(string name)
+        {
+            this.name = name;
+            items = new List<ItemTask>();
+        }
+
+        public Player(List<ItemTask> dbItems, string name)
+        {
+            this.name = name;
+            items = dbItems;
+        }
+
+        public async Task IncrementItem(string item)
+        {
+            ItemTask foundItem = getItem(item);
+
+            if (foundItem == null)
+                return;
+
+            foundItem.itemAmount++;
+        }
+
+
+        public async Task<bool> UpgradeGatheringLevel(string item, int amount)
+        {
+            ItemTask foundItem = getItem(item);
+
+            if (foundItem == null)
+                return false;
+
+            if (foundItem.upgradeGatheringLevelCost() > amount)
+                return false;
+
+            foundItem.resourceGatheringLevel++;
+
+            return true;
+        }
+
+        public async Task<List<ItemTask>> GetItems()
+        {
+            return items;
+        }
+
+        public ItemTask getItem(string name)
+        {
+            return items.Where(i => i.itemName == name).FirstOrDefault();
+        }
+
+        public ItemTask getEnabledTask()
+        {
+            return items.Where(i => i.enabled == true).FirstOrDefault();
+        }
+
+        public async Task<bool> switchEnabledTask(string name)
+        {
+            ItemTask currentlyEnabled = getEnabledTask();
+            ItemTask toBeEnabled = getItem(name);
+
+            if (currentlyEnabled == null || toBeEnabled == null)
+                return false;
+
+            currentlyEnabled.enabled = false;
+            toBeEnabled.enabled = true;
+
+            return true;
+
+        }
+    }
+
+
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using GameSystemObjects;
+
+namespace TestProject
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+
+            testIncrementItem_Correct();
+            testUpgradeGatheringLevel();
+            testNegativeUpgradeGatheringLevel();
+            testSwitchEnabledTask();
+            testNegativeSwitchEnabledTask();
+
+        }
+
+        static void testIncrementItem_Correct()
+        {
+            Player p = new Player(new List<ItemTask> {
+
+                new ItemTask { itemName = "Test", itemAmount = 0 },
+
+            }, "MyName");
+
+            _ = p.IncrementItem("Test");
+
+            if (p.getItem("Test").itemAmount == 1)
+                Console.WriteLine("testIncrementItem_Correct - Success");
+
+        }
+
+        static async void testUpgradeGatheringLevel()
+        {
+            Player p = new Player(new List<ItemTask> {
+
+                new ItemTask { itemName = "Test", itemAmount = 0, resourceGatheringLevel = 1, },
+
+            }, "MyName");
+            bool returned = await p.UpgradeGatheringLevel("Test", 2);
+
+            if (returned && p.getItem("Test").resourceGatheringLevel == 2)
+                Console.WriteLine("testUpgradeGatheringLevel - Success");
+        }
+
+        static async void testNegativeUpgradeGatheringLevel()
+        {
+            Player p = new Player(new List<ItemTask>
+            {
+                new ItemTask { itemName = "Test", itemAmount = 0, resourceGatheringLevel = 4},
+
+            }, "MyName");
+            bool returned = await p.UpgradeGatheringLevel("Test", 1);
+
+            if (!returned && p.getItem("Test").resourceGatheringLevel == 4)
+                Console.WriteLine("testNegativeUpgradeGatheringLevel - Success");
+
+        }
+
+        static async void testSwitchEnabledTask()
+        {
+            Player p = new Player(new List<ItemTask>
+            {
+                new ItemTask { itemName = "Test1", enabled = false, itemAmount = 0, resourceGatheringLevel = 4},
+                new ItemTask { itemName = "Test2", enabled = true, itemAmount = 0, resourceGatheringLevel = 4},
+
+            }, "MyName");
+
+            bool returned = await p.switchEnabledTask("Test1");
+
+            if (returned && p.getItem("Test1").enabled && !p.getItem("Test2").enabled)
+                Console.WriteLine("testSwitchEnabledTask - Success");
+        }
+        static async void testNegativeSwitchEnabledTask()
+        {
+            Player p = new Player(new List<ItemTask>
+            {
+
+            }, "MyName");
+
+            bool returned = await p.switchEnabledTask("Test");
+
+            if (!returned)
+                Console.WriteLine("testNegativeSwitchEnabledTask - Success");
+        }
+
+    }
+}
