0d4c976b416899048778ff009c03c28775e9cb6b
John Garner <66663652+JohnGarnerIII@users.noreply.github.com>
2021-02-27 19:43:23 -0500

    Update IPlayerRepository.cs

--- GameSystemObjects/Players/IPlayerRepository.cs
+++ GameSystemObjects/Players/IPlayerRepository.cs
@@ -7,7 +7,7 @@ namespace GameSystemObjects.Players
 {
     public interface IPlayerRepository
     {
-        
+        //Player Repository needs to be priotity #1
         public Task<Player> GetPlayer(string name);
 
         public Task SavePlayer(Player p);
