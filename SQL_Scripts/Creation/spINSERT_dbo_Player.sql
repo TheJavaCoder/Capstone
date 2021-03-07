f3e9dcbcaeb93fc701e28cb22b88e20be4c01b30
Placidie Mugabo <placidymuzima@gmail.com>
2021-03-06 21:06:17 -0500

    Update spINSERT_dbo_Player.sql
    updated to accommodate password column

--- SQL_Scripts/Creation/spINSERT_dbo_Player.sql
+++ SQL_Scripts/Creation/spINSERT_dbo_Player.sql
@@ -1,16 +1,18 @@
 USE [DB_111206_clicker]
 GO
 
-/****** Object:  StoredProcedure [dbo].[spINSERT_dbo_Player]    Script Date: 3/4/2021 6:34:30 PM ******/
+/****** Object:  StoredProcedure [dbo].[spINSERT_dbo_Player]    Script Date: 3/6/2021 9:05:19 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 CREATE PROCEDURE [dbo].[spINSERT_dbo_Player] 
 @player_ID int, 
-@username varchar(50)
+@username varchar(255),
+@password varchar(500)
 AS
 SET NOCOUNT ON
 -- 1 - Declare variables
@@ -21,13 +23,15 @@ INSERT INTO [dbo].[Player]
 
 ([player_ID]
 
-,[username])
+,[username],
+[password])
 
 VALUES
 
 (@player_ID
 
-,@username)
+,@username,
+@password)
 
 GO
 
