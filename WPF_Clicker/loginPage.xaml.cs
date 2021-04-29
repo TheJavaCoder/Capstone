311b2f245bd424cb6ae7ad50df9b087f54ef1f9d
T28ST <78190945+T28ST@users.noreply.github.com>
2021-04-29 19:32:25 -0400

    Add Enter key event for login boxes

--- WPF_Clicker/loginPage.xaml.cs
+++ WPF_Clicker/loginPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using GameSystemObjects.ControllerModels;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace WPF_Clicker
 {
@@ -37,6 +38,54 @@ namespace WPF_Clicker
             }
 
 
+        }
+
+        private async void Username_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                PlayerLoginModel plm = new PlayerLoginModel
+                {
+                    username = Username.Text,
+                    password = Password.Text
+                };
+
+                var p = await window.loginPlayerAsync(plm);
+                if (p != null)
+                {
+                    window.player = p;
+                    window.Content = new taskList(window);
+                }
+                else
+                {
+                    ErrorLabel.Content = "invaild login";
+                }
+
+            }
+        }
+
+        private async void Password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                PlayerLoginModel plm = new PlayerLoginModel
+                {
+                    username = Username.Text,
+                    password = Password.Text
+                };
+
+                var p = await window.loginPlayerAsync(plm);
+                if (p != null)
+                {
+                    window.player = p;
+                    window.Content = new taskList(window);
+                }
+                else
+                {
+                    ErrorLabel.Content = "invaild login";
+                }
+
+            }
         }
     }
 }
